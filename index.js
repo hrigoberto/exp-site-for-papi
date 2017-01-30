0aba8309cf345648e9aff51d8257d65330d3454f
jfkbeatkid <hernadezrigoberto@gmail.com>
2017-01-29 19:58:47 -0500

    added some routing

--- index.js
+++ index.js
@@ -6,6 +6,12 @@ app.use(express.static(__dirname + '/public'));
 app.get('/', function(req , res) {
   res.sendFile('/public/html/index.html', {root: __dirname});
 })
+app.get('/about', function(req , res) {
+  res.sendFile('/public/html/about.html', {root: __dirname});
+})
+app.get('/services', function(req , res) {
+  res.sendFile('/public/html/services.html', {root: __dirname});
+})
 
 app.listen(8080, function(){
   console.log('listening on port 8080');
